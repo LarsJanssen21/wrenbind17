1767f7557e6cd83fb34d7def20c238bfb07a7047
LarsJanssen21 <220015@buas.nl>
2025-02-24 15:31:08 +0100

    Updated modules

--- modules/FindWren.cmake
+++ modules/FindWren.cmake
@@ -2,14 +2,12 @@ include(FindPackageHandleStandardArgs)
 
 if(NOT TARGET Wren)
   find_path(WREN_INCLUDE_DIR NAMES wren.hpp PATHS ${CMAKE_CURRENT_LIST_DIR}/../libs/wren/src/include)
-  find_path(WREN_OPTIONAL_RANDOM_DIR NAMES wren_opt_random.h PATHS ${CMAKE_CURRENT_LIST_DIR}/../libs/wren/src/optional)
   mark_as_advanced(FORCE WREN_INCLUDE_DIR)
-  mark_as_advanced(FORCE WREN_OPTIONAL_RANDOM_DIR)
 
-  file(GLOB_RECURSE WREN_SOURCES [${CMAKE_CURRENT_SOURCE_DIR}/libs/wren/src/vm/*.c, ${CMAKE_CURRENT_SOURCE_DIR}/libs/wren/src/optional/*.c])
+  file(GLOB_RECURSE WREN_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/libs/wren/src/vm/*.c;${CMAKE_CURRENT_SOURCE_DIR}/libs/wren/src/optional/*.c)
   add_library(Wren STATIC ${WREN_SOURCES}) 
   target_compile_definitions(Wren PRIVATE WREN_OPT_META=0 WREN_OPT_RANDOM=1)
-  target_include_directories(Wren PUBLIC ${WREN_INCLUDE_DIR})
+  target_include_directories(Wren PUBLIC ${WREN_INCLUDE_DIR};${CMAKE_CURRENT_LIST_DIR}/../libs/wren/src/optional/*.h)
   set_target_properties(Wren PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${WREN_INCLUDE_DIR})
 endif()
 
