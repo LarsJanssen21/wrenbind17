528fde25b851a923c9b5e76fe4c53ff808941f10
LarsJanssen21 <220015@buas.nl>
2025-02-24 15:47:02 +0100

    update

--- modules/FindWren.cmake
+++ modules/FindWren.cmake
@@ -2,14 +2,12 @@ include(FindPackageHandleStandardArgs)
 
 if(NOT TARGET Wren)
   find_path(WREN_INCLUDE_DIR NAMES wren.hpp PATHS ${CMAKE_CURRENT_LIST_DIR}/../libs/wren/src/include)
-  set(WREN_VM_DIR $(CMAKE_CURRENT_LIST_DIR)/../libs/wren/src/vm)
-  set(WREN_OPTIONAL_DIR $(CMAKE_CURRENT_LIST_DIR)/../libs/wren/src/optional)
   mark_as_advanced(FORCE WREN_INCLUDE_DIR)
 
-  file(GLOB_RECURSE WREN_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/libs/wren/src/vm/*.c;${CMAKE_CURRENT_SOURCE_DIR}/libs/wren/src/optional/*.c)
+  file(GLOB_RECURSE WREN_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/libs/wren/src/vm/*.c)
   add_library(Wren STATIC ${WREN_SOURCES}) 
-  target_compile_definitions(Wren PRIVATE WREN_OPT_META=0 WREN_OPT_RANDOM=1)
-  target_include_directories(Wren PUBLIC ${WREN_INCLUDE_DIR};${WREN_VM_DIR};${WREN_OPTIONAL_DIR})
+  target_compile_definitions(Wren PRIVATE WREN_OPT_META=0 WREN_OPT_RANDOM=0)
+  target_include_directories(Wren PUBLIC ${WREN_INCLUDE_DIR})
   set_target_properties(Wren PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${WREN_INCLUDE_DIR})
 endif()
 
